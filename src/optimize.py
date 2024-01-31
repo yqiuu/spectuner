9c741072ca60baa119eb8f7414bdacce9a7305ed
邱逸盛 <yishengq@zhejianglab.com>
2024-01-31 11:03:07 +0800

    fix: Derive include_list correctly

--- src/optimize.py
+++ src/optimize.py
@@ -3,21 +3,24 @@ import numpy as np
 from .xclass_wrapper import create_wrapper_from_config
 
 
-def refine_molecules(params_list, mol_dict_list, include_list_list, config_xclass):
+def refine_molecules(params_list, mol_dict_list, segments_list, include_list_list, config_xclass):
     params_mol = []
     params_iso = []
     mol_dict_ret = {}
-    include_list_ret = [[] for _ in range(len(include_list_list[0]))]
-    for params, mol_dict, include_list in zip(params_list, mol_dict_list, include_list_list):
+    for params, mol_dict in zip(params_list, mol_dict_list):
         wrapper = create_wrapper_from_config(None, mol_dict, config_xclass)
         params_mol.append(wrapper.pm.get_all_mol_params(params))
         params_iso.append(wrapper.pm.get_all_iso_params(params))
         mol_dict_ret.update(mol_dict)
-        for list_ret, list_new in zip(include_list_ret, include_list):
-            list_ret.extend(list_new)
     params_mol = np.concatenate(params_mol)
     params_iso = np.concatenate(params_iso)
     params = np.append(params_mol, params_iso)
+
+    n_segment = max([max(segment) for segment in segments_list]) + 1
+    include_list_ret = [[] for _ in range(n_segment)]
+    for segment, include_list in zip(segments_list, include_list_list):
+        for i_segment, mol_list in zip(segment, include_list):
+            include_list_ret[i_segment].extend(mol_list)
     return params, mol_dict_ret, include_list_ret
 
 
