ba1cf620a6ccabecfdbfb6de414d864f2d9a8d8c
邱逸盛 <yishengq@zhejianglab.com>
2024-03-12 15:22:01 +0800

    refactor: Remove unused codes

--- src/fitting_model.py
+++ src/fitting_model.py
@@ -1,45 +1,7 @@
-from typing import Any
 import numpy as np
 
-from .xclass_wrapper import create_wrapper_from_config, derive_freq_range
+from .xclass_wrapper import derive_freq_range
 from .algorithms import derive_median_frac_threshold, PeakMatchingLoss
-
-
-def create_fitting_model(obs_data, mol_list, include_list,
-                         config_xclass, config_opt,
-                         vLSR=None, tBack=None, loss_fn=None,
-                         base_data=None):
-    kwargs = {}
-    if vLSR is not None:
-        kwargs["vLSR"] = vLSR
-    if tBack is not None:
-        kwargs["tBack"] = tBack
-    wrapper = create_wrapper_from_config(obs_data, mol_list, config_xclass, **kwargs)
-
-    scaler = ScalerExtra(wrapper.pm)
-    bounds = scaler.derive_bounds(
-        config_opt["bounds_mol"],
-        config_opt["bounds_iso"],
-        config_opt["bounds_misc"]
-    )
-
-    if loss_fn is not None:
-        pass
-    elif config_opt["loss_fn"] == "l1":
-        loss_fn = l1_loss
-    elif config_opt["loss_fn"] == "l1_log":
-        loss_fn = l1_loss_log
-    elif config_opt["loss_fn"] == "l2":
-        loss_fn = l2_loss
-    elif config_opt["loss_fn"] == "l2_log":
-        loss_fn = l2_loss_log
-    else:
-        raise ValueError("Unknown loss function.")
-    return FittingModel(
-        obs_data, wrapper, include_list, bounds, scaler, loss_fn,
-        **config_opt.get("kwargs", {}),
-        base_data=base_data
-    )
 
 
 def l1_loss(y_pred, y_obs):
@@ -60,42 +22,6 @@ def l2_loss(y_pred, y_obs):
 def l2_loss_log(y_obs, y_pred):
     delta = y_obs - y_pred
     return np.mean(np.log(1 + np.square(delta)))
-
-
-class Scaler:
-    def __init__(self, n_mol_param, n_param_per_mol):
-        self.n_mol_param = n_mol_param
-        self.n_param_per_mol = n_param_per_mol
-
-    def call(self, params):
-        # Use log scale for the first three parameters.
-        # source_size, T_rot, n_tot
-        # params_mol (N_mol, N_params)
-        params_mol, params_misc = self.split_params(params)
-        params_mol = params_mol.copy()
-        params_mol[:, :3] = 10**params_mol[:, :3]
-        params_new = np.append(np.ravel(params_mol), params_misc)
-        return params_new
-
-    def derive_bounds(self, bounds):
-        bounds = bounds.copy()
-        lb = bounds[:, 0]
-        ub = bounds[:, 1]
-        lb_mol, lb_misc = self.split_params(lb)
-        lb_mol[:, :3] = np.log10(lb_mol[:, :3])
-        lb_new = np.append(lb_mol, lb_misc)
-        ub_mol, ub_misc = self.split_params(ub)
-        ub_mol[:, :3] = np.log10(ub_mol[:, :3])
-        ub_new = np.append(ub_mol, ub_misc)
-        bounds_new = np.vstack([lb_new, ub_new]).T
-        return bounds_new
-
-    def split_params(self, params):
-        # params (N,)
-        params_mol = params[:self.n_mol_param]
-        params_mol = params_mol.reshape(-1, self.n_param_per_mol)
-        params_misc = params[self.n_mol_param:]
-        return params_mol, params_misc
 
 
 class FittingModel:
