dfe29ff36b585810d965c766f0f1215338747ba8
邱逸盛 <yishengq@zhejianglab.com>
2024-03-25 11:59:56 +0800

    build: Packaging

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    "numpy>=1.23",
+    "scipy>=1.10",
+    "pandas>=2.0",
+]
+
+# Get version
+exec(open('meteor/version.py', 'r').read())
+#
+setup(
+    name='meteor',
+    version=__version__,
+    author='Yisheng Qiu',
+    author_email="hpc_yqiuu@163.com",
+    install_requires=install_requires,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
