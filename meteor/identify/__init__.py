f08c1bef2e891034115847ba0f1e256e467709df
邱逸盛 <yishengq@zhejianglab.com>
2024-04-11 14:26:42 +0800

    refactor: Rearange the functions into different files

--- meteor/identify/__init__.py
+++ meteor/identify/__init__.py
@@ -1,2 +1,3 @@
 from .identify import *
 from .ident_result import *
+from .peaks import *
--- meteor/identify/ident_result.py
+++ meteor/identify/ident_result.py
@@ -1,10 +1,49 @@
+from functools import partial
 from collections import defaultdict
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from copy import deepcopy
 
+import numpy as np
 import pandas as pd
 
-from . import identify
+
+def compute_T_single_data(mol_store, config_slm, params, freq_data):
+    pm = mol_store.create_parameter_manager(config_slm)
+    T_single_data = defaultdict(dict)
+    for item in mol_store.mol_list:
+        for mol in item["molecules"]:
+            params_single = pm.get_single_params(mol, params)
+            mol_store_single = mol_store.select_single(mol)
+            T_single_data[item["id"]][mol] \
+                = mol_store_single.compute_T_pred_data(params_single, freq_data, config_slm)
+    T_single_data = dict(T_single_data)
+    return T_single_data
+
+
+def sum_T_single_data(T_single_dict, T_back, key=None):
+    # Get a test dict
+    for sub_dict in T_single_dict.values():
+        for T_single_data in sub_dict.values():
+            break
+        break
+    T_ret_data = [None for _ in T_single_data]
+
+    def sum_sub(target_dict):
+        for T_single_data in target_dict.values():
+            for i_segment, T_single in enumerate(T_single_data):
+                if T_single is None:
+                    continue
+                if T_ret_data[i_segment] is None:
+                    T_ret_data[i_segment] = T_back
+                T_ret_data[i_segment] = T_ret_data[i_segment] + T_single - T_back
+
+    if key is not None:
+        sum_sub(T_single_dict[key])
+        return T_ret_data
+
+    for sub_dict in T_single_dict.values():
+        sum_sub(sub_dict)
+    return T_ret_data
 
 
 def concat_identify_result(res_list):
@@ -34,10 +73,45 @@ def concat_identify_result(res_list):
 
 
 @dataclass
+class LineTable:
+    freq: np.ndarray = field(default_factory=partial(np.zeros, 0))
+    loss: np.ndarray = field(default_factory=partial(np.zeros, 0))
+    score: np.ndarray = field(default_factory=partial(np.zeros, 0))
+    frac: np.ndarray = field(default_factory=list)
+    id: np.ndarray = field(default_factory=list)
+    name: np.ndarray = field(default_factory=list)
+    error: np.ndarray = field(default_factory=partial(np.zeros, 0))
+    norm: np.ndarray = field(default_factory=partial(np.zeros, 0))
+
+    def append(self, line_table, sparsity=None):
+        self.freq = np.append(self.freq, line_table.freq)
+
+        for name in ["loss", "score", "error", "norm"]:
+            if sparsity is None:
+                arr_new = getattr(line_table, name)
+            else:
+                inds, num = sparsity
+                arr_tmp = getattr(line_table, name)
+                arr_new = np.full(num, np.nan)
+                arr_new[inds] = arr_tmp
+            setattr(self, name, np.append(getattr(self, name), arr_new))
+
+        for name in ["frac", "id", "name"]:
+            if sparsity is None:
+                list_new = getattr(line_table, name)
+            else:
+                inds, num = sparsity
+                list_new = [None for _ in range(num)]
+                for idx, val in zip(inds, getattr(line_table, name)):
+                    list_new[idx] = val
+            getattr(self, name).extend(list_new)
+
+
+@dataclass
 class IdentResult:
     df_mol: pd.DataFrame
-    line_table: object
-    line_table_fp: object
+    line_table: LineTable
+    line_table_fp: LineTable
     T_single_dict: dict
     freq_data: list
     T_back: float
@@ -113,7 +187,7 @@ class IdentResult:
     def get_T_pred(self, key=None, name=None):
         if key is not None and name is not None:
             return self.T_single_dict[key][name]
-        return identify.sum_T_single_data(self.T_single_dict, self.T_back, key)
+        return sum_T_single_data(self.T_single_dict, self.T_back, key)
 
     def filter_name_list(self, target_set, name_list):
         inds = []
