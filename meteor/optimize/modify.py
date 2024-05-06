4149665bde046ba148bf4bbff2d52aa5ac15f65e
邱逸盛 <yishengq@zhejianglab.com>
2024-05-06 11:29:05 +0800

    fix: Fix bugs in modify

--- meteor/optimize/modify.py
+++ meteor/optimize/modify.py
@@ -9,7 +9,6 @@ __all__ = ["modify"]
 
 
 def modify(config, parent_dir):
-    config_slm = config["sl_model"]
     config_modify = config["modify"]
     exclude_id_list = config_modify["exclude_id_list"]
     exclude_name_set = set(config_modify["exclude_name_list"])
@@ -29,7 +28,7 @@ def modify(config, parent_dir):
         names_in.extend(mols)
 
     mol_store_new, params_new = mol_store.select_subset_with_params(
-        names_in, data_combine["params_best"], config_slm
+        names_in, data_combine["params_best"], config
     )
 
     include_id_list = config_modify["include_id_list"]
@@ -42,13 +41,13 @@ def modify(config, parent_dir):
             params = data["params_best"]
             names_in = set(mol_store.mol_list[0]["molecules"]) - exclude_name_set
             mol_store, params \
-                = mol_store.select_subset_with_params(names_in, params, config_slm)
+                = mol_store.select_subset_with_params(names_in, params, config)
             mol_store_list.append(mol_store)
             params_list.append(params)
         mol_store_new, params_new \
             = combine_mol_stores(mol_store_list, params_list)
 
-    T_pred_data = mol_store_new.compute_T_pred_data(params_new, freq_data, config_slm)
+    T_pred_data = mol_store_new.compute_T_pred_data(params_new, freq_data, config)
     save_dict = {
         "mol_store": mol_store_new,
         "freq": freq_data,
