0698845e995006b877a5ed8ab5110afd3ecc48eb
邱逸盛 <yishengq@zhejianglab.com>
2024-07-23 09:19:32 +0800

    feat: Update load_preprocess

--- meteor/preprocess.py
+++ meteor/preprocess.py
@@ -1,17 +1,38 @@
-import glob
+from copy import deepcopy
 
 import numpy as np
 
 
-def load_preprocess(file_spec, T_back):
-    if isinstance(file_spec, str) and file_spec.startswith("glob:"):
-        file_spec = glob.glob(file_spec.replace("glob:", ""))
-    elif isinstance(file_spec, str):
-        file_spec = [file_spec]
+def load_preprocess(obs_data, T_back):
+    """Load observed spectra.
 
-    obs_data = [np.loadtxt(fname) for fname in file_spec]
-    obs_data = [preprocess_spectrum(spec, T_back) for spec in obs_data]
-    return obs_data
+    The function resets the intensity below ``T_back`` to ``T_back``.
+
+    Args:
+        obs_data (str or list): Data of observed spectra.
+            - str: Load the text file specified by the string.
+            - list of str: Load every text file specified by the string.
+            - list of array: Each array should be a spectrum. The first colunm
+            gives the frequency (MHz) and the second column gives the intensity
+            (K).
+        T_back (float): Background temperature.
+
+    Returns:
+        list: Each element specifies a spectrum. The first colunm gives the
+            frequency (MHz) and the second column gives the intensity (K).
+    """
+    if isinstance(obs_data, str):
+        obs_data = [obs_data]
+
+    obs_data_ret = []
+    for item in obs_data:
+        if isinstance(item, str):
+            obs_data_ret.append(np.loadtxt(item))
+        else:
+            obs_data_ret.append(deepcopy(item))
+
+    obs_data_ret = [preprocess_spectrum(spec, T_back) for spec in obs_data_ret]
+    return obs_data_ret
 
 
 def preprocess_spectrum(spec_obs, temp_back):
