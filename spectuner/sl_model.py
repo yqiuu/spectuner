1b7a77e719e3aabb1d5f5d63f92f1c1f1d7277bc
邱逸盛 <iyishengq@zhejianglab.org>
2024-08-21 16:42:33 +0800

    fix: Fix a bug in prepare_effective_spectra

--- spectuner/sl_model.py
+++ spectuner/sl_model.py
@@ -121,19 +121,16 @@ def prepare_effective_spectra(freq_list, freqs_fine, spectra_fine):
         spec_eff = [0.]*len(freqs)
         for i_freq, (lower, upper) in enumerate(zip(freqs_p[:-1], freqs_p[1:])):
             idx_b, idx_e = np.searchsorted(freqs_fine, [lower, upper])
-            if idx_e == idx_b:
-                spec_eff[i_freq] = 0.
-            else:
-                x_freq = np.zeros(idx_e - idx_b + 2)
-                x_freq[1:-1] = freqs_fine[idx_b:idx_e]
-                x_freq[0] = lower
-                x_freq[-1] = upper
-                y_spec = np.zeros(idx_e - idx_b + 2)
-                y_spec[1:-1] = spectra_fine[idx_b:idx_e]
-                y_lower, y_upper = np.interp([lower, upper], freqs_fine, spectra_fine)
-                y_spec[0] = y_lower
-                y_spec[-1] = y_upper
-                spec_eff[i_freq] = np.trapz(y_spec, x_freq)/(upper - lower)
+            x_freq = np.zeros(idx_e - idx_b + 2)
+            x_freq[1:-1] = freqs_fine[idx_b:idx_e]
+            x_freq[0] = lower
+            x_freq[-1] = upper
+            y_spec = np.zeros(idx_e - idx_b + 2)
+            y_spec[1:-1] = spectra_fine[idx_b:idx_e]
+            y_lower, y_upper = np.interp([lower, upper], freqs_fine, spectra_fine)
+            y_spec[0] = y_lower
+            y_spec[-1] = y_upper
+            spec_eff[i_freq] = np.trapz(y_spec, x_freq)/(upper - lower)
         spec_list.append(np.asarray(spec_eff))
     return spec_list
 
