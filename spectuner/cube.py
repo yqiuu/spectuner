b5c434408b65543dbe7f316a12154c86cf7d013d
邱逸盛 <yishengq@zhejianglab.com>
2025-04-28 09:19:10 +0800

    refactor: Add and use CubePipline

--- spectuner/cube.py
+++ spectuner/cube.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from typing import Optional
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 
 import h5py
 import numpy as np
@@ -13,165 +13,23 @@ from tqdm import tqdm
 from .sl_model import const_factor_mu_sigma
 
 
-def convert_cube_data(cube_helper: CubeHelper,
-                      file_list: list,
-                      header_lookup: dict,
-                      save_name: str,
-                      noise_factor,
-                      rel_height: float= 0.5,
-                      fname_mask=None,
-                      v_LSR: float=0.,
-                      kwargs_filter=None):
-    if kwargs_filter is None:
-        kwargs_filter = {}
-
-    header_list = cube_helper.prepare_header_list(file_list, header_lookup)
-    cube_helper.validate_freq_order(header_list)
-
-    counts = None
-    if fname_mask is None:
-        cond_mask = None
-    else:
-        mask = np.squeeze(fits.open(fname_mask)[0].data)
-        cond_mask = ~np.isnan(mask)
-
-    for item in file_list:
-        data_contiuum = np.squeeze(fits.open(item["contiuum"])[0].data) # (W, H)
-        n_row, n_col = data_contiuum.shape
-        if cond_mask is None:
-            cond_mask = np.full(data_contiuum.shape, True)
-        cond_mask &= ~np.isnan(data_contiuum)
-        cond_mask &= ~np.isinf(data_contiuum)
-    inds_mask = np.where(cond_mask)
-
-    T_obs_data = []
-    T_bg_data = []
-    freq_data = []
-    noise_data = []
-    beam_data = []
-    mask_data = []
-    for item, header in zip(file_list, header_list):
-        hdul = fits.open(item["line"])[0]
-        # (1, C, W, H) > (C, W, H)
-        data_line = np.squeeze(hdul.data)
-        data_line = np.transpose(data_line, axes=(1, 2, 0))
-        data_line = data_line[inds_mask]
-
-        hdul = fits.open(item["contiuum"])[0]
-        data_contiuum = np.squeeze(hdul.data) # (W, H)
-        data_contiuum = data_contiuum[inds_mask]
-
-        # Fix nan and inf values
-        n_pixel = len(inds_mask[0])
-        num = 0
-        for idx, spec in tqdm(
-            enumerate(data_line),
-            desc="Checking invalid values...",
-            total=len(data_line)
-        ):
-            cond = np.isnan(spec) | np.isinf(spec)
-            num += np.count_nonzero(cond)
-            data_line[idx, cond] = data_contiuum[idx]
-        print("Fraction of nan and inf values: {:.1f}% ({}/{})".format(
-            num/n_pixel*100., num, n_pixel))
-
-        # Estimate RMS noise
-        print("Estimating noise...")
-        noise = cube_helper.estimate_noise(data_line)
-        freqs = cube_helper.load_freqs(header, v_LSR)
-        freq_m = .5*(freqs[0] + freqs[-1])
-        bmaj = header["BMAJ"]
-        bmin = header["BMIN"]
-
-        #
-        if freqs[1] < freqs[0]:
-            freqs = freqs[::-1]
-            data_line = data_line[:, ::-1]
-
-        # Fix outliers
-        factor_out = 1e4
-        num = 0
-        for idx, spec in tqdm(
-            enumerate(data_line),
-            desc="Checking invalid values...",
-            total=len(data_line)
-        ):
-            cond = spec > factor_out*noise
-            num += np.count_nonzero(cond)
-            data_line[idx, cond] = data_contiuum[idx]
-        print("Fraction of outliers: {:.1f}% ({}/{})".format(
-            num/n_pixel*100., num, n_pixel))
-
-        # Count number of lines
-        counts = np.zeros(len(data_contiuum), dtype="i8")
-        prominence = noise_factor*noise
-        pbar = tqdm(
-            enumerate(data_line),
-            desc="Counting peaks...",
-            total=len(data_line)
-        )
-        for idx, spec in pbar:
-            peaks = signal.find_peaks(
-                np.maximum(spec, 0.), # Neglect absoprtion
-                prominence=prominence,
-                rel_height=rel_height
-            )[0]
-            counts[idx] += len(peaks)
-
-        # Convert unit
-        T_bg = to_kelvin(data_contiuum, freq_m, bmaj, bmin)
-        T_obs = to_kelvin(data_line, freqs, bmaj, bmin)
-        noise = to_kelvin(noise, freq_m, bmaj, bmin)
-        beam_info = np.array([bmaj, bmin])
-
-        # Prepare data to be saved
-        T_obs_data.append(T_obs)
-        T_bg_data.append(T_bg)
-        freq_data.append(freqs)
-        noise_data.append(noise)
-        beam_data.append(beam_info)
-        mask_data.append(item.get("mask", None))
-
-    #
-    T_obs_data, \
-    T_bg_data, \
-    freq_data, \
-    noise_data, \
-    beam_data = cube_helper.split_by_mask(
-        T_obs_data, T_bg_data, freq_data, noise_data, beam_data, mask_data
-    )
-
-    #
-    cond = cube_helper.filter_pixel(T_obs_data, noise_data, counts)
-    if np.count_nonzero(cond) == 0:
-        print("No acceptable pixels in {}.".format(save_name))
-        return
-    T_obs_data = [T_obs_cube[cond] for T_obs_cube in T_obs_data]
-    T_bg_data = [T_bg_cube[cond] for T_bg_cube in T_bg_data]
-    counts = counts[cond]
-    inds_mask = np.vstack(inds_mask).T
-    inds_mask = inds_mask[cond]
-
-    print("Saving data...")
-    with h5py.File(save_name, "w") as fp:
-        fp.attrs["noise_factor"] = float(noise_factor)
-        fp.attrs["rel_height"] = rel_height
-        fp.attrs["n_row"] = n_row
-        fp.attrs["n_col"] = n_col
-        fp.attrs["max_count"] = max(counts)
-        for key, val in kwargs_filter.items():
-            fp.attrs[key] = val
-        fp.create_dataset("count", data=counts, dtype="i4")
-        fp.create_dataset("index", data=inds_mask, dtype="i4")
-        grp_cube = fp.create_group("cube")
-        cube_helper.save_file(
-            grp_cube, T_obs_data, T_bg_data, freq_data, noise_data, beam_data
-        )
-
-
 @dataclass(frozen=True)
-class CubeHelper:
-    """Class that provides various functions to process the cube data.
+class CubePipeline:
+    """Convert cube data into format that can be used by the model.
+
+    For each segment, the pipline does the following steps:
+        1. Set any inf and nan values in the spectrum to the continuum.
+        2. Estimate the global RMS noise of the spectrum by randomly selecting
+            several pixels and using sigma clipping.
+        3. Set any outiler values (> 1e4*noise) to the continuum.
+        4. Estimate the number of peaks in each spectrum.
+
+    Secondly, the pipline removes bad fixels:
+        1. Estimate the local RMS noise using the spectrum of one pixel.
+        2. Count the number of peaks, and remove the pixel if the total number
+            of peaks of all segments is below a threshold.
+        3. Estimate the plateau fraction using by smoothing spectrum, and
+            remove the pixel if the plateau fraction is above a threshold.
 
     Args:
         maxiters: Maximum number of iterations used in ``sigma_clipped_stats``.
@@ -179,16 +37,166 @@ class CubeHelper:
         atol_factor: Absolute tolerance factor to remove plateau.
         window_size: Window size to smooth the spectrum when removing plateau.
         f_pla_cut: Plateau fraction below which a pixel is removed.
-        noise_factor: RMS noise multipler to identify peaks.
+        noise_factor_global: Global RMS noise multipler to identify peaks.
+        noise_factor_local: Local RMS noise multipler to identify peaks.
         number_cut: Number of peak below which a pixel is removed.
     """
     maxiters: int = 1000
     n_estimate: int = 10000
     atol_factor: float = 1.e-4
     window_size: int = 31
-    noise_factor: float = 4.
+    rel_height: float = 0.25
+    noise_factor_global: float = 4.
+    noise_factor_local: float = 1.
     f_pla_cut: float = .75
     number_cut: int = 3
+    v_LSR: float = 0.
+
+    def run(self,
+            file_list: list,
+            header_lookup: dict,
+            save_name: str,
+            fname_mask=None):
+        header_list = self.prepare_header_list(file_list, header_lookup)
+        self.validate_freq_order(header_list)
+
+        counts = None
+        if fname_mask is None:
+            cond_mask = None
+        else:
+            mask = np.squeeze(fits.open(fname_mask)[0].data)
+            cond_mask = ~np.isnan(mask)
+
+        for item in file_list:
+            data_contiuum = np.squeeze(fits.open(item["contiuum"])[0].data) # (W, H)
+            n_row, n_col = data_contiuum.shape
+            if cond_mask is None:
+                cond_mask = np.full(data_contiuum.shape, True)
+            cond_mask &= ~np.isnan(data_contiuum)
+            cond_mask &= ~np.isinf(data_contiuum)
+        inds_mask = np.where(cond_mask)
+
+        T_obs_data = []
+        T_bg_data = []
+        freq_data = []
+        noise_data = []
+        beam_data = []
+        mask_data = []
+        for item, header in zip(file_list, header_list):
+            hdul = fits.open(item["line"])[0]
+            # (1, C, W, H) > (C, W, H)
+            data_line = np.squeeze(hdul.data)
+            data_line = np.transpose(data_line, axes=(1, 2, 0)) # (W, H, C)
+            data_line = data_line[inds_mask] # (N, C)
+
+            hdul = fits.open(item["contiuum"])[0]
+            data_contiuum = np.squeeze(hdul.data) # (W, H)
+            data_contiuum = data_contiuum[inds_mask]
+
+            # Fix nan and inf values
+            num_tot = np.prod(data_line.shape)
+            num = 0
+            for idx, spec in tqdm(
+                enumerate(data_line),
+                desc="Checking invalid values...",
+                total=len(data_line)
+            ):
+                cond = np.isnan(spec) | np.isinf(spec)
+                num += np.count_nonzero(cond)
+                data_line[idx, cond] = data_contiuum[idx]
+            print("Fraction of nan and inf values: {:.1f}% ({}/{})".format(
+                num/num_tot*100., num, num_tot))
+
+            # Estimate RMS noise
+            print("Estimating noise...")
+            noise = self.estimate_noise(data_line)
+            freqs = self.load_freqs(header)
+            freq_m = .5*(freqs[0] + freqs[-1])
+            bmaj = header["BMAJ"]
+            bmin = header["BMIN"]
+
+            #
+            if freqs[1] < freqs[0]:
+                freqs = freqs[::-1]
+                data_line = data_line[:, ::-1]
+
+            # Fix outliers
+            factor_out = 1e4
+            num = 0
+            for idx, spec in tqdm(
+                enumerate(data_line),
+                desc="Checking invalid values...",
+                total=len(data_line)
+            ):
+                cond = spec > factor_out*noise
+                num += np.count_nonzero(cond)
+                data_line[idx, cond] = data_contiuum[idx]
+            print("Fraction of outliers: {:.1f}% ({}/{})".format(
+                num/num_tot*100., num, num_tot))
+
+            # Count number of lines
+            counts = np.zeros(len(data_contiuum), dtype="i8")
+            prominence = self.noise_factor_global*noise
+            pbar = tqdm(
+                enumerate(data_line),
+                desc="Counting peaks...",
+                total=len(data_line)
+            )
+            for idx, spec in pbar:
+                peaks = signal.find_peaks(
+                    np.maximum(spec, 0.), # Neglect absoprtion
+                    prominence=prominence,
+                    rel_height=self.rel_height
+                )[0]
+                counts[idx] += len(peaks)
+
+            # Convert unit
+            T_bg = to_kelvin(data_contiuum, freq_m, bmaj, bmin)
+            T_obs = to_kelvin(data_line, freqs, bmaj, bmin)
+            noise = to_kelvin(noise, freq_m, bmaj, bmin)
+            beam_info = np.array([bmaj, bmin])
+
+            # Prepare data to be saved
+            T_obs_data.append(T_obs)
+            T_bg_data.append(T_bg)
+            freq_data.append(freqs)
+            noise_data.append(noise)
+            beam_data.append(beam_info)
+            mask_data.append(item.get("mask", None))
+
+        #
+        T_obs_data, \
+        T_bg_data, \
+        freq_data, \
+        noise_data, \
+        beam_data = self.split_by_mask(
+            T_obs_data, T_bg_data, freq_data, noise_data, beam_data, mask_data
+        )
+
+        #
+        cond = self.filter_pixel(T_obs_data, noise_data, counts)
+        if np.count_nonzero(cond) == 0:
+            print("No acceptable pixels in {}.".format(save_name))
+            return
+        T_obs_data = [T_obs_cube[cond] for T_obs_cube in T_obs_data]
+        T_bg_data = [T_bg_cube[cond] for T_bg_cube in T_bg_data]
+        counts = counts[cond]
+        inds_mask = np.vstack(inds_mask).T
+        inds_mask = inds_mask[cond]
+
+        print("Saving data...")
+        with h5py.File(save_name, "w") as fp:
+            fp.attrs["n_row"] = n_row
+            fp.attrs["n_col"] = n_col
+            fp.attrs["max_count"] = max(counts)
+            for key, val in asdict(self).items():
+                fp.attrs[key] = val
+            fp.create_dataset("count", data=counts, dtype="i4")
+            fp.create_dataset("index", data=inds_mask, dtype="i4")
+            grp_cube = fp.create_group("cube")
+            self.save_file(
+                grp_cube, T_obs_data, T_bg_data, freq_data, noise_data, beam_data
+            )
 
     def prepare_header_list(self, file_list, header_lookup):
         header_list = []
@@ -224,7 +232,7 @@ class CubeHelper:
         msg = "Frequencies of the segments must be in ascending order."
         assert np.all(np.diff(freqs) > 0), msg
 
-    def load_freqs(self, header, v_LSR):
+    def load_freqs(self, header):
         """Load frequency data from a FITS file.
 
         Args:
@@ -239,7 +247,7 @@ class CubeHelper:
         freq_end = freq_start + (num - 1)*dfreq
         freqs = np.linspace(freq_start, freq_end, num)
         freqs *= 1e-6 # Hz to MHz
-        freqs *= (1. + const_factor_mu_sigma()[0]*v_LSR)
+        freqs *= (1. + const_factor_mu_sigma()[0]*self.v_LSR)
         return freqs
 
     def estimate_noise(self, data):
@@ -271,8 +279,9 @@ class CubeHelper:
                 )
                 peaks, _ = signal.find_peaks(
                     T_obs - median,
-                    height=self.noise_factor*std,
-                    prominence=self.noise_factor*std
+                    height=self.noise_factor_local*std,
+                    prominence=self.noise_factor_local*std,
+                    rel_height=self.rel_height
                 )
                 counts_sub[idx] += len(peaks)
             cond &= f_pla >= self.f_pla_cut
