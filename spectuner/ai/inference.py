f69af18b28dc52de7110361017aebfda540a22d0
邱逸盛 <yishengq@zhejianglab.com>
2025-05-08 13:56:53 +0800

    chore: Fix a wrong ident

--- spectuner/ai/inference.py
+++ spectuner/ai/inference.py
@@ -188,27 +188,27 @@ def collate_fn_padding(batch):
 
 
 def create_obs_info_from_cube(fname: str, idx_pixel:int , misc_data: list):
-        T_obs_data = []
-        T_bg_data = []
-        with h5py.File(fname) as fp:
-            for i_segment in range(len(fp["cube"])):
-                grp = fp["cube"][str(i_segment)]
-                T_obs_data.append(np.array(grp["T_obs"][idx_pixel]))
-                T_bg_data.append(np.array(grp["T_bg"][idx_pixel]))
-        freq_data, noise_data, beam_data = misc_data
+    T_obs_data = []
+    T_bg_data = []
+    with h5py.File(fname) as fp:
+        for i_segment in range(len(fp["cube"])):
+            grp = fp["cube"][str(i_segment)]
+            T_obs_data.append(np.array(grp["T_obs"][idx_pixel]))
+            T_bg_data.append(np.array(grp["T_bg"][idx_pixel]))
+    freq_data, noise_data, beam_data = misc_data
 
-        obs_info = []
-        for i_segment, freq in enumerate(freq_data):
-            spec = np.vstack([freq, T_obs_data[i_segment]]).T
-            spec = preprocess_spectrum(spec)
-            obs_info.append({
-                "spec": spec,
-                "beam_info": beam_data[i_segment],
-                "T_bg": T_bg_data[i_segment],
-                "need_cmb": True,
-                "noise": noise_data[i_segment],
-            })
-        return obs_info
+    obs_info = []
+    for i_segment, freq in enumerate(freq_data):
+        spec = np.vstack([freq, T_obs_data[i_segment]]).T
+        spec = preprocess_spectrum(spec)
+        obs_info.append({
+            "spec": spec,
+            "beam_info": beam_data[i_segment],
+            "T_bg": T_bg_data[i_segment],
+            "need_cmb": True,
+            "noise": noise_data[i_segment],
+        })
+    return obs_info
 
 
 def load_misc_data(fname):
