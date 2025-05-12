05af98aff689ab2c26d3b7a9b257804828dc8574
邱逸盛 <yishengq@zhejianglab.com>
2025-05-12 10:01:11 +0800

    feat: Update load_preprocess

--- spectuner/preprocess.py
+++ spectuner/preprocess.py
@@ -4,16 +4,13 @@ import numpy as np
 def load_preprocess(obs_info, clip=True):
     """Load observed spectra.
 
-    1. Ensure the frequency is ascending.
-    2. Resets the intensity below zero to zero.
+    The following preprocessing is performed:
+        1. Ensure the frequency is ascending.
+        2. Reset the intensity below zero to zero.
 
     Args:
         obs_info (list): A list of dicts that specify the observation
             information.
-            - If ``spec`` in the dict, load the corresponding array,
-                with the first column being the frequency (MHz) and the second
-                column being the intensity (K).
-            - If ``fname`` in the dict, load the corresponding text file.
 
     Returns:
         list: Each element specifies a spectrum. The first colunm gives the
@@ -23,13 +20,15 @@ def load_preprocess(obs_info, clip=True):
     for item in obs_info:
         if "spec" in item:
             obs_data.append(np.copy(item["spec"]))
-        elif "fname" in item:
-            obs_data.append(np.loadtxt(item["fname"]))
     return [preprocess_spectrum(spec, clip) for spec in obs_data]
 
 
 def preprocess_spectrum(spectrum, clip=True):
     """Preprocess spectrum
+
+    The following preprocessing is performed:
+        1. Ensure the frequency is ascending.
+        2. Reset the intensity below zero to zero.
 
     Args:
         spectrum (array): (N, 2). The first colunm gives the frequency and
