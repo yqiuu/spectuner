964855d583d8d753f18b5ae5c968449d7657997d
邱逸盛 <yishengq@zhejianglab.org>
2025-07-09 11:18:53 +0800

    test: Use new convention for params_info

--- spectuner/tests/test_parameter_manager.py
+++ spectuner/tests/test_parameter_manager.py
@@ -15,15 +15,17 @@ def test_derive_recover(theta_info):
         "T_ex": {"is_shared": True, "is_log": False},
         "N_tot": {"is_shared": True, "is_log": True},
         "delta_v": {"is_shared": False, "is_log": True},
-        "v_LSR": {"is_shared": False, "is_log": False},
+        "v_offset": {"is_shared": False, "is_log": False},
     }
     bounds_info = {
         "theta": [-3, -0.00043],
         "T_ex":  [1.0, 1000.0],
         "N_tot": [12.0, 20.0],
         "delta_v": [-0.5, 1.5],
-        "v_LSR": [-12., 12.],
+        "v_offset": [-12., 12.],
     }
+    for key, bound in bounds_info.items():
+        params_info[key]["bound"] = bound
     specie_list = [
         {"id": 0, "root": None, "species": ["A", "B"]},
         {"id": 1, "root": None, "species": ["C", "D", "E"]},
@@ -36,7 +38,7 @@ def test_derive_recover(theta_info):
 
     param_mgr = spectuner.ParameterManager(specie_list, params_info, obs_info)
     rstate = np.random.RandomState(825)
-    lb, ub = param_mgr.derive_bounds(bounds_info).T
+    lb, ub = param_mgr.derive_bounds().T
     params = lb + (ub - lb)*rstate.rand(len(ub))
     params_mol = param_mgr.derive_params(params)
     params_re = param_mgr.recover_params(params_mol)
