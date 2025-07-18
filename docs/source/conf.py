e469008b96521148319cd0330409a0ee1268b619
邱逸盛 <yishengq@zhejianglab.org>
2025-07-18 13:51:08 +0800

    docs: Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,11 +18,11 @@ sys.path.insert(0, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------
 
 project = 'Spectuner'
-copyright = '2024, Yisheng Qiu'
+copyright = '2025, Yisheng Qiu'
 author = 'Yisheng Qiu'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '2.0.0b1'
 
 
 # -- General configuration ---------------------------------------------------
